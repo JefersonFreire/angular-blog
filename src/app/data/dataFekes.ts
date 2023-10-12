bc9d2ce9ba2603f0e5532c22a949dbdfe21d519f
JefersonFreire <jfreire86@hotmail.com>
2023-10-12 18:08:11 -0300

    content created

---
+++ src/app/data/dataFekes.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO ANUNCIADO",
+    "description": "Marvel studios anuncia novo filme do Homem de Ferro",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title": "Nova série anunciada no Disney +",
+    "description": "ola",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+]
